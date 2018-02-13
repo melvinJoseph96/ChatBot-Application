7532d9edfd4165f744ec1004f10ae79257a2215b
Leon <leontodd0@gmail.com>
2018-02-13 16:08:00 +0000

    added message class and controller class for dealing with messages sent by the user via spring

---
+++ src/main/java/CO2015/group9/chatbot/Message.java
@@ -0,0 +1,32 @@
+package CO2015.group9.chatbot;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private String message;
+    private String timestamp;
+
+    public Message() {
+    }
+
+    public Message(String message, String timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
