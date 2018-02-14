7ffc8c0a86961aceea0bc9abb3ee5c03f1ad43e6
Holly <hollysmith823@gmail.com>
2018-02-14 15:59:04 +0000

    fixed groovy errors and completed spring framework test for 5 second pop up delay

--- src/test/groovy/CO2015/group9/chatbot/ChatbotApplicationTests.groovy
+++ src/test/groovy/CO2015/group9/chatbot/ChatbotApplicationTests.groovy
@@ -1,17 +1,14 @@
 package CO2015.group9.chatbot
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.test.context.web.WebAppConfiguration
@@ -21,9 +18,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@ContextConfiguration
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(controllers=[IndexController.class])
+@ContextConfiguration
 class ChatbotApplicationTests extends Specification {
 	@Autowired
 	WebApplicationContext wac
@@ -31,16 +29,14 @@ class ChatbotApplicationTests extends Specification {
 	MockMvc mockMvc
 	ResultActions result
 
-
-//    @Test
-//    def "Response for HTTP request '/'"() {
-//        given: "the context of the controller is set up"
-//        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build()
-//        when: "I do a get '/'"
-//        result = this.mockMvc.perform(get('/'))
-//        then: "I should see the view 'index'"
-//        result.andExpect(view().name('index.html'))
-//    }
-
+    @Test
+    def "Response for HTTP request '/'"() {
+        given: "the context of the controller is set up"
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build()
+        when: "I do a get '/'"
+        result = this.mockMvc.perform(get('/'))
+        then: "I should see the view 'index'"
+        result.andExpect(status().isOk()).andExpect(view().name('index.html'))
+    }
 
 }
