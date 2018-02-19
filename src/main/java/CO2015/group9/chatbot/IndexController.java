7eff359ffa57a5662cbb515e785df8d42f95207c
Holly <hollysmith823@gmail.com>
2018-02-19 12:15:18 +0000

    added changes to admin page

--- src/main/java/CO2015/group9/chatbot/IndexController.java
+++ src/main/java/CO2015/group9/chatbot/IndexController.java
@@ -1,8 +1,10 @@
 package CO2015.group9.chatbot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -21,8 +23,16 @@ public class IndexController {
     public String register(){
         return "register.html";
     }
-
     @RequestMapping(value = "/controlpanel", method = RequestMethod.GET)
-    public String controlPanel() { return "adminPage.html";}
+    public String controlPanel(){
+        return "adminPage.html";
+    }
+//    @RequestMapping(value = "/controlpanel", method = RequestMethod.POST)
+//    public String controlPanel(@ModelAttribute("intent") Intent intent, @RequestParam(value="id", required=false, defaultValue="-1") int id) {
+//       if (id>=0){
+//
+//       }
+//        return "adminPage.html";
+//    }
 
 }
--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -4,3 +4,18 @@ function search() {
 function login(){
 
 }
+
+function addRow(){
+    var table = document.getElementById("intents");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 =row.insertCell(2);
+    var cell4 =row.insertCell(3);
+    var cell5 =row.insertCell(4);
+    cell1.innerHTML = "aaa";
+    cell2.innerHTML = "bbb";
+    cell3.innerHTML = "ccc";
+    cell4.innerHTML = "<button>/<button>";
+    cell5.innerHTML = "<button>X<button>";
+}
