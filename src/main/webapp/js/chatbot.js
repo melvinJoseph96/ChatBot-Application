ccc3298399517aa5818abf19a8a92a9ec8da2e16
dd186 <dd186@student.le.ac.uk>
2018-02-26 17:33:58 +0000

    Added code for the story: "A user can select the best way for them to be contacted"

--- src/main/webapp/js/chatbot.js
+++ src/main/webapp/js/chatbot.js
@@ -14,36 +14,7 @@ $(function() { // On page load
 	        	if(inputMessage.length !== 0 && inputMessage.trim()){
 	        		addMessage("user", inputMessage); // Display the sent message in the chat box
                     $('#input').val(""); // Clear the message text box ready for another message
-                    $.ajax({ // Send the message to spring for processing
-                        type: "POST",
-                        url: "chatbot",
-                        data: JSON.stringify({
-                            "message": inputMessage
-                        }),
-                        datatype: "json",
-                        contentType: "application/json",
-                        success: function(data) {
-
-                            var sound = document.getElementById("messageReceived");
-                            sound.play();
-
-                            if (data.message == "Which team you want to send an email to?"){
-                                addMessage("bot",data.message );
-                                addMessage("bot", data.message.replace(data.message,'<button type="button" onclick="location.href=\'mailto:sales@example.com\'"> Sales </button>' +
-                                    ' <button type="button" onclick="location.href=\'mailto:marketing@example.com\'"> Marketing</button>'+
-                                    ' <button type="button" onclick="location.href=\'mailto:finance@example.com\'"> Finance</button>' +
-                                    ' <button type="button" onclick="location.href=\'mailto:it@example.com\'"> IT </button>' +
-                                    ' <button type="button" onclick="location.href=\'mailto:hr@example.com\'"> HR</button>'+
-                                    ' <button type="button" onclick="location.href=\'mailto:recruitment@example.com\'"> Recruitment</button>'+
-                                    ' <button type="button" onclick="location.href=\'mailto:other@example.com\'"> Other</button>' ))
-
-                            }
-                        //data.message.replace(data.message,'<a href="mailto:someone@example.com" >Click here to send an email.</a>' ))
-                            else
-                            addMessage("bot", data.message) // Display the response message in the chat box
-                            $('#messages').scrollTop($('#messages')[0].scrollHeight); // Make sure the chatbox is scrolled to the bottom
-                        }
-                    });
+                    processing(inputMessage)
 	        	}
 	    	}
 	    }
@@ -67,4 +38,58 @@ function reopen(){
     $('#titlebar').fadeIn(); //Add titlebar to screen
     $('#input').fadeIn(); //Add text box to screen
     $('#messages').fadeIn(); //Add messages to screen
+}
+
+
+function processing(inputMessage){
+    $.ajax({
+        type: "POST",
+        url: "chatbot",
+        data: JSON.stringify({
+            "message": inputMessage
+        }),
+        datatype: "json",
+        contentType: "application/json",
+        success: function(data) {
+
+            var sound = document.getElementById("messageReceived");
+            sound.play()
+
+            if (data.message == "Which team you want to send an email to?"){
+                addMessage("bot",data.message )
+                addMessage("bot", data.message.replace(data.message,'<button type="button" onclick="location.href=\'mailto:sales@example.com\'"> Sales </button>' +
+                    ' <button type="button" onclick="location.href=\'mailto:marketing@example.com\'"> Marketing</button>'+
+                    ' <button type="button" onclick="location.href=\'mailto:finance@example.com\'"> Finance</button>' +
+                    ' <button type="button" onclick="location.href=\'mailto:it@example.com\'"> IT </button>' +
+                    ' <button type="button" onclick="location.href=\'mailto:hr@example.com\'"> HR</button>'+
+                    ' <button type="button" onclick="location.href=\'mailto:recruitment@example.com\'"> Recruitment</button>'+
+                    ' <button type="button" onclick="location.href=\'mailto:other@example.com\'"> Other</button>' ))
+
+            }
+            else if (data.message == "How do you want to contact us?"){
+                addMessage("bot", data.message)
+                addMessage("bot",data.message.replace(data.message,'<button type="button" onclick= "email()" > Email </button>' +
+                    ' <button type="button" onclick="other()"> Other.. </button>'))
+            }
+            else if (data.message == "Contact us"){
+                addMessage("bot", data.message)
+                addMessage("bot",data.message.replace(data.message,'<p>London <br> 020 3056 8240 <br> Cottons Centre, Cottons Lane <br>' +
+                    ' London, SE1 2QG <br> <br> Leeds <br> 0113 331 5048 <br> No. 1 Whitehall Riverside <br> Leeds, West Yorkshire LS1 4BN <br> <br>' +
+                    ' Glasgow <br> 0141 218 3100 <br> 1 West Regent Street, 6th Floor <br> Glasgow, G2 1RW</p>'))
+            }
+            else
+                addMessage("bot", data.message) // Display the response message in the chat box
+            $('#messages').scrollTop($('#messages')[0].scrollHeight); // Make sure the chatbox is scrolled to the bottom
+        }
+    });
+}
+
+function email() {
+    var inputMessage = "email"
+    processing(inputMessage)
+}
+
+function other() {
+    var inputMessage = "phone"
+    processing(inputMessage)
 }
