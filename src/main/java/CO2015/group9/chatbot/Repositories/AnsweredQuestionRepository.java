a499da70919e3a22b58b24dfdd676007f93608e5
Holly <hollysmith823@gmail.com>
2018-03-12 20:57:02 +0000

    introduced a repository for answered questions

---
+++ src/main/java/CO2015/group9/chatbot/Repositories/AnsweredQuestionRepository.java
@@ -0,0 +1,8 @@
+package CO2015.group9.chatbot.Repositories;
+
+import CO2015.group9.chatbot.domain.AnsweredQuestion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnsweredQuestionRepository extends CrudRepository<AnsweredQuestion, Integer> {
+
+}
