409b2275233c967914087ed4a3250f5c7ca02eb3
Holly <hollysmith823@gmail.com>
2018-03-13 16:03:29 +0000

    multiple options can be edited at once

--- src/main/webapp/WEB-INF/js/admin.js
+++ src/main/webapp/WEB-INF/js/admin.js
@@ -204,32 +204,32 @@ function update(name,id){
                 }
             }
 
-            var userList ="<input type='text' id='textUs' placeholder='Enter new keywords' onkeypress='handle(event)' " +
+            var userList ="<input type='text' id='textUs"+userS.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' placeholder='Enter new keywords' onkeypress='handle(event,\""+userS.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"\")' " +
                  "name='keywords' size='45'><br>" +
 
-                "<input type='text' id='textUs1' placeholder='Edit selected keyword' onkeypress='handle1(event)' " +
+                "<input type='text' id='textUs1"+userS.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' placeholder='Edit selected keyword' onkeypress='handle1(event,\""+userS.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"\")' " +
                 "name='keywords1' size='45'>" +
 
-                "<br><select id='user_says' style='width: 333px;outline: none;' onclick='select(this)'>";
+                "<br><select id='user_says"+userS.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' style='width: 333px;outline: none;' onclick='select(this,\""+userS.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"\")'>";
             for (var j=0;j<userS.length;j++){ // list all the keywords into a dropdown
                 userList += "<option value='"+ userS[j]+"'>"+userS[j]+"</option>";
             }
             userList +=   "</select>";
 
-            var respList ="<input type='text' id='textR' placeholder='Enter new responses' size='45' onkeypress='handler(event)'>" +
-               "<br><input type='text' id='textResp1' placeholder='Edit selected response' onkeypress='handle2(event)' " +
+            var respList ="<input type='text' id='textR"+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' placeholder='Enter new responses' size='45' onkeypress='handler(event,\""+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"\")'>" +
+               "<br><input type='text' id='textResp1"+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' placeholder='Edit selected response' onkeypress='handle2(event,\""+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"\")' " +
                 "name='responses1' size='45'>" +
-                "<select style='width: 333px;outline: none;' id='responsesSelect' onclick='select1(this)'>";
+                "<select style='width: 333px;outline: none;' id='responsesSelect"+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' onclick='select1(this,\""+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"\")'>";
             for (var j=0;j<resp.length;j++){ //list all the responses into a dropdown
                respList += "<option onclick='this.parentElement.removeChild(this);' value='"+ resp[j]+"'>"+resp[j]+"</option>";
             }
             respList +=   "</select>";
 
-            td0.innerHTML = "<input type='text' size='40' value='"+name+ "'>"; // change the cells to input boxes with the values inside
+            td0.innerHTML = "<input id='name"+resp.toString().replace(/\s/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")+"' type='text' size='40' value='"+name+ "'>"; // change the cells to input boxes with the values inside
             tr[i].getElementsByTagName("td")[1].innerHTML = userList;
             tr[i].getElementsByTagName("td")[2].innerHTML = respList;
             tr[i].getElementsByTagName("td")[3].innerHTML = "<button id='update' onclick='submit2(this,\"" + id + "\")' style='background-color: #212121;border-radius: 15px;padding:5px;font-size: 10px'>Update</button>";
-
+            console.log(document.body.innerHTML);
             return false;
         }
     }
@@ -408,51 +408,51 @@ function cancel(){
     $('#addedResp').fadeOut();
 }
 
-function handle(e){
+function handle(e,a){
     if(e.keyCode === 13){ // if enter hit
         var option = document.createElement("option");
         option.onclick = function () { // add the onclick delete
             this.parentElement.removeChild(this);
         };
-        option.value = document.getElementById("textUs").value; // get the new value
-        option.innerText = document.getElementById("textUs").value; // get the new value
-        document.getElementById("textUs").value = ""; // remove text box value
-        document.getElementById("user_says").appendChild(option); // add new option to dropdown
+        option.value = document.getElementById("textUs"+a).value; // get the new value
+        option.innerText = document.getElementById("textUs"+a).value; // get the new value
+        document.getElementById("textUs"+a).value = ""; // remove text box value
+        document.getElementById("user_says"+a).appendChild(option); // add new option to dropdown
     }
 }
-function handle1(e){
+function handle1(e,a){
     if(e.keyCode === 13){ // if enter hit
-        var select= document.getElementById("user_says"); // get the select box
+        var select= document.getElementById("user_says"+a); // get the select box
         var option = select.options[select.selectedIndex];
-        option.value = document.getElementById("textUs1").value; // get the new value
-        option.text = document.getElementById("textUs1").value; // get the new text
-        document.getElementById("textUs1").value = ""; // remove text box value
+        option.value = document.getElementById("textUs1"+a).value; // get the new value
+        option.text = document.getElementById("textUs1"+a).value; // get the new text
+        document.getElementById("textUs1"+a).value = ""; // remove text box value
     }
 }
-function handle2(e){
+function handle2(e,a){
     if(e.keyCode === 13){ // if enter hit
-        var select= document.getElementById("responsesSelect"); // get the select box
+        var select= document.getElementById("responsesSelect"+a); // get the select box
         var option = select.options[select.selectedIndex];
-        option.value = document.getElementById("textResp1").value; // get the new value
-        option.text = document.getElementById("textResp1").value; // get the new text
-        document.getElementById("textResp1").value = ""; // remove text box value
+        option.value = document.getElementById("textResp1"+a).value; // get the new value
+        option.text = document.getElementById("textResp1"+a).value; // get the new text
+        document.getElementById("textResp1"+a).value = ""; // remove text box value
     }
 }
-function handler(e){
+function handler(e,a){
     if(e.keyCode === 13){ // if enter hit
         var option = document.createElement("option");
         option.onclick = function () { // add the onclick delete
             this.parentElement.removeChild(this);
         };
-        option.value = document.getElementById("textR").value; // get the new value
-        option.innerText = document.getElementById("textR").value; // get the new value
-        document.getElementById("textR").value = ""; // remove text box value
-        document.getElementById("responsesSelect").appendChild(option); // add new option to dropdown
+        option.value = document.getElementById("textR"+a).value; // get the new value
+        option.innerText = document.getElementById("textR"+a).value; // get the new value
+        document.getElementById("textR"+a).value = ""; // remove text box value
+        document.getElementById("responsesSelect"+a).appendChild(option); // add new option to dropdown
     }
 }
-function select(e){
-    document.getElementById("textUs1").value = e.options[e.selectedIndex].text; // put the selected value into the text box
+function select(e,a){
+    document.getElementById("textUs1"+a).value = e.options[e.selectedIndex].text; // put the selected value into the text box
 }
-function select1(e){
-    document.getElementById("textResp1").value = e.options[e.selectedIndex].text; // put the selected value into the text box
+function select1(e,a){
+    document.getElementById("textResp1"+a).value = e.options[e.selectedIndex].text; // put the selected value into the text box
 }
